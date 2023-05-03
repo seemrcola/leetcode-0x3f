1c48b9db6f43c9389f0cfb6fa12a81b1e3cc6dc6
seemrcola <seemrcola@163.com>
2023-05-03 22:51:18 +0800

    init

---
+++ slide-window/209/209.ts
@@ -0,0 +1,18 @@
+function minSubArrayLen(target: number, nums: number[]): number {
+  let ans = nums.length + 1
+  let left = 0 
+  let right = 0
+  let total = 0
+  for(right; right < nums.length; right++) {
+      total += nums[right]
+      while(total - nums[left] >= target) {
+          total -= nums[left]
+          left += 1    
+      }
+      if(total >= target)
+          ans = Math.min(ans, right - left + 1)        
+  }
+
+  if(ans == nums.length + 1) ans = 0
+  return ans
+};
